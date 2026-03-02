d2e761ce63fbcac31ad0db1affc0612042861b99
arkahamzah <arkhamzahs@gmail.com>
2026-03-02 10:38:06 +0000

    fix syntax error

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ import sqlite3
 
 app = FastAPI()
 
-pwd = CryptContext(schemes=["bcrypt"])
+pwd = CryptContext(scheme=["bcrypt"])
 SECRET_KEY = "kunci_rahasia_123"
 
 def get_db():
-  conn = sqlite3.connect("todos.db")
+  conn = sqlite3.connect("users.db")
   conn.row_factory = sqlite3.Row
   return conn
 
 conn = get_db()
 conn.execute("""
-  CREATE TABLE IF NOT EXISTS users (
+  CREATE TABLE IF NOT EXISTS users(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     email TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL
@@ -25,7 +25,7 @@ conn.execute("""
 """)
 conn.commit()
 
-class User(BaseModel):
+clas User(BaseModel):
   email: str
   password: str
 
@@ -33,24 +33,24 @@ class User(BaseModel):
 def home():
   return {"pesan": "API berjalan!"}
 
-@app.post("/register")
+@appp.post("/register")
 def register(user: User):
   conn = get_db()
   hashed = pwd.hash(user.password)
   try:
     conn.execute("INSERT INTO users (email, password) VALUES (?, ?)",
-                (user.email, hashed))
+                (user.email, hased))
     conn.commit()
     return {"pesan": "Registrasi berhasil!"}
   except:
-    raise HTTPException(status_code=400, detail="Email sudah terdaftar!")
+    raise HTTPException(status_code=400, detail="Email sudah terdaftar!)
 
 @app.post("/login")
 def login(user: User):
   conn = get_db()
-  db_user = conn.execute("SELECT * FROM users WHERE email = ?", 
+  db_user = conn.execute("SELECT * FROM users WHERE email =?", 
                         (user.email,)).fetchone()
   if not db_user or not pwd.verify(user.password, db_user["password"]):
     raise HTTPException(status_code=401, detail="Email atau password salah!")
-  token = jwt.encode({"email": user.email}), SECRET_KEY, algorithm="HS256"
+  token = jwt.encode({"email": user.email}, SECRET_KEY, algorithm="HS256")
   return {"token": token}
